811a9ec52044be58ed54bd4a06e2754197e44d95
Quartz <skyu.dev@gmail.com>
2022-10-24 23:31:12 +0900

    refactor: Separate HomeIcons from Home component

---
+++ src/Components/HomeIcons.tsx
@@ -0,0 +1,71 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import { IconDefinition, IconLookup } from "@fortawesome/fontawesome-svg-core";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Icons = styled.div`
+  font-size: 72px;
+  width: 150px;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 30px;
+  margin-top: 30px;
+`;
+// Fontawsome icons
+const BLANK_FACE_ICON = regular("face-meh-blank");
+const SMILE_FACE_ICON = solid("face-smile");
+const DIZZY_FACE_ICON = solid("face-dizzy");
+// types
+interface IconInterface {
+  color: string;
+  iconName: IconDefinition;
+}
+
+function HomeIcons() {
+  // Icon states
+  const [firstIconState, setFirstIconState] = useState<boolean>(false);
+  const [secondIconState, setSecondIconState] = useState<boolean>(false);
+
+  // Create Icons
+  let firstIcon: IconInterface = {
+    color: "black",
+    iconName: BLANK_FACE_ICON,
+  };
+  let secondIcon: IconInterface = {
+    color: "black",
+    iconName: BLANK_FACE_ICON,
+  };
+  // Toggle icons by clicking event
+  if (firstIconState) {
+    firstIcon.color = "#6BCB77";
+    firstIcon.iconName = SMILE_FACE_ICON;
+  }
+  if (secondIconState) {
+    secondIcon.color = "#FF6B6B";
+    secondIcon.iconName = DIZZY_FACE_ICON;
+  }
+  const toggleIcon = (icon: "first" | "second") => {
+    if (icon === "first") {
+      setFirstIconState((prev) => !prev);
+    } else {
+      setSecondIconState((prev) => !prev);
+    }
+  };
+  return (
+    <Icons>
+      <FontAwesomeIcon
+        icon={firstIcon.iconName}
+        onClick={toggleIcon.bind(null, "first")}
+        style={{ color: firstIcon.color, cursor: "pointer" }}
+      />
+      <FontAwesomeIcon
+        icon={secondIcon.iconName}
+        onClick={toggleIcon.bind(null, "second")}
+        style={{ color: secondIcon.color, cursor: "pointer" }}
+      />
+    </Icons>
+  );
+}
+export default HomeIcons;
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
-import { IconDefinition, IconLookup } from "@fortawesome/fontawesome-svg-core";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { currRouteState, Quiz, quizDataState, routesState } from "../atom";
+import HomeIcons from "../Components/HomeIcons";
 
 // Style Component
 const Wrapper = styled.div`
@@ -19,15 +17,7 @@ const Title = styled.h1`
   font-weight: 700;
   margin-bottom: 5px;
 `;
-const Icons = styled.div`
-  font-size: 72px;
-  width: 150px;
-  display: flex;
-  justify-content: center;
-  gap: 20px;
-  margin-bottom: 30px;
-  margin-top: 30px;
-`;
+
 const StartBtn = styled.button`
   background-color: ${(props) => props.theme.btnColor};
   border: none;
@@ -39,47 +29,11 @@ const StartBtn = styled.button`
   cursor: pointer;
 `;
 
-// Fontawsome icons
-const BLANK_FACE_ICON = regular("face-meh-blank");
-const SMILE_FACE_ICON = solid("face-smile");
-const DIZZY_FACE_ICON = solid("face-dizzy");
-// types
-interface IconInterface {
-  color: string;
-  iconName: IconDefinition;
-}
-
 function Home() {
   const navigate = useNavigate();
   const routes = useRecoilValue<string[]>(routesState);
   const [currRoute, setCurrRoute] = useRecoilState<number>(currRouteState);
-  const [firstIconState, setFirstIconState] = useState<boolean>(false);
-  const [secondIconState, setSecondIconState] = useState<boolean>(false);
 
-  let firstIcon: IconInterface = {
-    color: "black",
-    iconName: BLANK_FACE_ICON,
-  };
-  let secondIcon: IconInterface = {
-    color: "black",
-    iconName: BLANK_FACE_ICON,
-  };
-  // Toggle icons by clicking event
-  if (firstIconState) {
-    firstIcon.color = "#6BCB77";
-    firstIcon.iconName = SMILE_FACE_ICON;
-  }
-  if (secondIconState) {
-    secondIcon.color = "#FF6B6B";
-    secondIcon.iconName = DIZZY_FACE_ICON;
-  }
-  const toggleIcon = (icon: "first" | "second") => {
-    if (icon === "first") {
-      setFirstIconState((prev) => !prev);
-    } else {
-      setSecondIconState((prev) => !prev);
-    }
-  };
   // Navigation logic
   useEffect(() => {
     setCurrRoute(0);
@@ -95,18 +49,7 @@ function Home() {
       <span>
         어린 시절 쌍둥이들의 사진을 보고 누가 서경(Quartz)인지 맞춰보세요!
       </span>
-      <Icons>
-        <FontAwesomeIcon
-          icon={firstIcon.iconName}
-          onClick={toggleIcon.bind(null, "first")}
-          style={{ color: firstIcon.color, cursor: "pointer" }}
-        />
-        <FontAwesomeIcon
-          icon={secondIcon.iconName}
-          onClick={toggleIcon.bind(null, "second")}
-          style={{ color: secondIcon.color, cursor: "pointer" }}
-        />
-      </Icons>
+      <HomeIcons />
       <div style={{ height: "15px" }}>
         {/* Load start button after route state loaded*/}
         {routes.length === 0 ? (

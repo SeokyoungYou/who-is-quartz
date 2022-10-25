e7eb5d2ff42d6d98da166f62d988e4b90a46e13e
Quartz <skyu.dev@gmail.com>
2022-10-25 23:14:37 +0900

    feat: Implement scores in Result screen

--- src/Routes/ResultScreen.tsx
+++ src/Routes/ResultScreen.tsx
@@ -2,15 +2,33 @@ import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useResetRecoilState } from "recoil";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 import styled from "styled-components";
-import { scoreState } from "../atom";
+import { Score, scoreState } from "../atom";
 
 const Wrapper = styled.div``;
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 500;
+`;
+const Results = styled.div``;
+const Retry = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+`;
 function ResultScreen() {
   const navigate = useNavigate();
   const resetScores = useResetRecoilState(scoreState);
+  const scores = useRecoilValue<Score[]>(scoreState);
+  // const scores = [{ 1: false }, { 2: true }, { 3: false }, { 4: false }];
+  const countCorrect = scores.reduce((acc, score) => {
+    const [answer] = Object.values(score);
+    if (answer === true) return acc + 1;
+    else return acc;
+  }, 0);
+  // console.log(scores);
   // Navigate to home
   const homeClicked = () => {
     resetScores();
@@ -18,14 +36,22 @@ function ResultScreen() {
   };
   return (
     <Wrapper>
-      <Title>ResultScreen</Title>
-
-      <FontAwesomeIcon
-        icon={solid("house")}
-        onClick={homeClicked}
-        style={{ fontSize: "32px", cursor: "pointer" }}
-      />
-      <h1>재도전하기</h1>
+      <Title>당신은 4 문제 중 {countCorrect} 문제를 맞췄습니다!</Title>
+      <Results>
+        <span>내 결과 업로드하고 순위 확인하기</span>
+        <form>
+          <input type="text" />
+          <button type="submit">submit</button>
+        </form>
+        <div>Data from firebase</div>
+      </Results>
+      <Retry onClick={homeClicked}>
+        <FontAwesomeIcon
+          icon={solid("arrow-rotate-right")}
+          style={{ fontSize: "32px", cursor: "pointer" }}
+        />
+        <h1>재도전하기</h1>
+      </Retry>
     </Wrapper>
   );
 }
